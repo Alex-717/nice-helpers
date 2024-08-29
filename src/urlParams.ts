d6edbea2d9d8a9bafe4ebe1e18ac8a6f4f38a265
Alex-717 <liangyuhao717@163.com>
2024-08-29 19:48:11 +0800

    设置url参数

--- src/urlParams.ts
+++ src/urlParams.ts
@@ -31,3 +31,32 @@ function getParamByReg (name: string, url?: string) {
   const result = urlStr.match(reg)
   return result ? result[2] : ''
 }
+
+export const setParam = (function () {
+  if (hasURLFunction()) {
+    return setParamByURL
+  } else {
+    return setParamByReg
+  }
+})();
+function setParamByURL (name: string, val: string, url: string) {
+  const urlStr = url ? url : hasLocation() ? location.href : ''
+  if (!urlStr) return false
+  const urlObj = new URL(urlStr)
+  const params = urlObj.searchParams
+  params.set(name, val)
+  return urlObj.toString()
+}
+
+// 已有参数就更新，没有则添加
+function setParamByReg (name: string, val: string, url: string) {
+  const urlStr = url ? url : hasLocation() ? location.href : ''
+  if (!urlStr) return false
+  const reg = new RegExp(`(\\?|&)${name}=.*?(&|#|$)`)
+  if (urlStr.match(reg)) {
+    return urlStr.replace(reg, "$1" + name + "=" + val + "$2")
+  }
+  const urlArr = url.split('#')
+  urlArr[0] = `${urlArr[0]}${urlArr[0].indexOf('?') > -1 ? '&' : '?'}${name}=${val}`
+  return urlArr.join('#')
+}
